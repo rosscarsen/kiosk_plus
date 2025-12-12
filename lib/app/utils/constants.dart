7b154123d3a8e6e1a25099efcdc6db939673e176
rosscarsen <rosscarsen@gmail.com>
2025-12-12 13:26:30 +0800

    feat(样式): 添加必填提示颜色常量

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -7,4 +7,5 @@ class AppColors {
   static const Color kTextSub = Color(0xFF55585e); // 副文字
   static const Color kPrice = Color(0xFFFF1a6c); // 价格
   static const Color kLine = Color(0xFFF1F5F9); // 分割线
+  static const Color kRequire = Color(0xFFFF4D4F); // 必填提示
 }
