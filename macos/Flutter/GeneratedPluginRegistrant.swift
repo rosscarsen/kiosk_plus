82ab2961858b0301f4d54addbd2db3794183a15d
rosscarsen <rosscarsen@gmail.com>
2025-12-22 16:18:15 +0800

    refactor: 移除flutter_inappwebview插件依赖
    清理不再使用的flutter_inappwebview插件相关配置，包括Windows和macOS平台的注册代码

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_settings
-import flutter_inappwebview_macos
 import network_info_plus
 import sqflite_darwin
 import universal_ble
@@ -14,7 +13,6 @@ import webcrypto
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
-  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UniversalBlePlugin.register(with: registry.registrar(forPlugin: "UniversalBlePlugin"))
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,15 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_inappwebview_windows/flutter_inappwebview_windows_plugin_c_api.h>
 #include <flutter_pos_printer_platform_image_3_sdt/flutter_pos_printer_platform_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <universal_ble/universal_ble_plugin_c_api.h>
 #include <webcrypto/webcrypto_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterInappwebviewWindowsPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterInappwebviewWindowsPluginCApi"));
   FlutterPosPrinterPlatformPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterPosPrinterPlatformPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
