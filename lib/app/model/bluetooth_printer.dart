752f0a44c054aa26ccf26f6de9822ff94fea7dce
rosscarsen <rosscarsen@gmail.com>
2025-12-26 10:25:11 +0800

    refactor(model): 将蓝牙打印机模型从JSON序列化迁移到Hive存储
    重构BluetoothPrinter模型，使用Hive替代json_annotation进行序列化
    新增port/vendorId/productId/isBle/state字段，并设置默认值
    实现TypeAdapter以支持Hive的二进制序列化

--- lib/app/model/bluetooth_printer.dart
+++ lib/app/model/bluetooth_printer.dart
@@ -1,16 +1,44 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:hive_ce/hive.dart';
+
+import '../../hive_type_ids.dart';
 
 part 'bluetooth_printer.g.dart';
 
-@JsonSerializable(explicitToJson: true)
+@HiveType(typeId: HiveTypeIds.innerPrinterInfo)
 class BluetoothPrinter {
-  @JsonKey(name: "deviceName")
+  @HiveField(0)
   String? deviceName;
-  @JsonKey(name: "address")
+  @HiveField(1)
   String? address;
-  BluetoothPrinter({this.deviceName, this.address});
+  @HiveField(2, defaultValue: "9100")
+  String? port;
+  @HiveField(3)
+  String? vendorId;
+  @HiveField(4)
+  String? productId;
+  @HiveField(5, defaultValue: false)
+  bool? isBle;
+  @HiveField(6)
+  bool? state;
+  BluetoothPrinter({this.deviceName, this.address, this.port, this.vendorId, this.productId, this.isBle, this.state});
 
-  factory BluetoothPrinter.fromJson(Map<String, dynamic> json) => _$BluetoothPrinterFromJson(json);
+  factory BluetoothPrinter.fromJson(Map<String, dynamic> json) => BluetoothPrinter(
+    deviceName: json["deviceName"] as String?,
+    address: json["address"] as String?,
+    port: json["port"] as String?,
+    vendorId: json["vendorId"] as String?,
+    productId: json["productId"] as String?,
+    isBle: json["isBle"] as bool?,
+    state: json["state"] as bool?,
+  );
 
-  Map<String, dynamic> toJson() => _$BluetoothPrinterToJson(this);
+  Map<String, dynamic> toJson() => {
+    "deviceName": deviceName,
+    "address": address,
+    "port": port,
+    "vendorId": vendorId,
+    "productId": productId,
+    "isBle": isBle,
+    "state": state,
+  };
 }
--- lib/app/model/bluetooth_printer.g.dart
+++ lib/app/model/bluetooth_printer.g.dart
@@ -3,17 +3,57 @@
 part of 'bluetooth_printer.dart';
 
 // **************************************************************************
-// JsonSerializableGenerator
+// TypeAdapterGenerator
 // **************************************************************************
 
-BluetoothPrinter _$BluetoothPrinterFromJson(Map<String, dynamic> json) =>
-    BluetoothPrinter(
-      deviceName: json['deviceName'] as String?,
-      address: json['address'] as String?,
-    );
+class BluetoothPrinterAdapter extends TypeAdapter<BluetoothPrinter> {
+  @override
+  final typeId = 10;
 
-Map<String, dynamic> _$BluetoothPrinterToJson(BluetoothPrinter instance) =>
-    <String, dynamic>{
-      'deviceName': instance.deviceName,
-      'address': instance.address,
+  @override
+  BluetoothPrinter read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
+    return BluetoothPrinter(
+      deviceName: fields[0] as String?,
+      address: fields[1] as String?,
+      port: fields[2] == null ? '9100' : fields[2] as String?,
+      vendorId: fields[3] as String?,
+      productId: fields[4] as String?,
+      isBle: fields[5] == null ? false : fields[5] as bool?,
+      state: fields[6] as bool?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, BluetoothPrinter obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.deviceName)
+      ..writeByte(1)
+      ..write(obj.address)
+      ..writeByte(2)
+      ..write(obj.port)
+      ..writeByte(3)
+      ..write(obj.vendorId)
+      ..writeByte(4)
+      ..write(obj.productId)
+      ..writeByte(5)
+      ..write(obj.isBle)
+      ..writeByte(6)
+      ..write(obj.state);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BluetoothPrinterAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
